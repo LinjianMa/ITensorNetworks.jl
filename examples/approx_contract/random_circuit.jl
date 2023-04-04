ea9b1c67ae9464bf7d6b9f9184b53ef8b09d01fd
LinjianMa <malinjianev@gmail.com>
2023-04-04 16:56:17 -0500

    Add factorization in random_circuit

--- examples/approx_contract/random_circuit.jl
+++ examples/approx_contract/random_circuit.jl
@@ -11,7 +11,6 @@ include("bench.jl")
 
 Random.seed!(1234)
 TimerOutputs.enable_debug_timings(ITensorNetworks)
-# TimerOutputs.enable_debug_timings(@__MODULE__)
 ITensors.set_warn_order(100)
 
 function gate_tensors(hilbert::Vector{<:Index}, gates; applydag=false)
@@ -22,9 +21,15 @@ function gate_tensors(hilbert::Vector{<:Index}, gates; applydag=false)
       t = dag(gate(hilbert, g))
       inds1 = [hilbert[n] for n in g[2]]
       t = swapinds(t, inds1, [i' for i in inds1])
-      push!(Us, t)
     else
-      push!(Us, gate(hilbert, g))
+      t = gate(hilbert, g)
+    end
+    if length(g[2]) == 2
+      ind = hilbert[g[2][1]]
+      L, R = factorize(t, ind, ind'; cutoff=1e-16)
+      push!(Us, L, R)
+    else
+      push!(Us, t)
     end
     for n in g[2]
       hilbert[n] = hilbert[n]'
@@ -34,18 +39,6 @@ function gate_tensors(hilbert::Vector{<:Index}, gates; applydag=false)
   @info "hilbert", hilbert
   return Us, hilbert
 end
-
-# function line_partition_gates(N, gates; order="row")
-#   @assert order in ["row", "column"]
-#   nrow, ncol = N
-#   if order == "row"
-#     partition_qubits = [[(c - 1) * nrow + r for r in 1:nrow] for c in 1:ncol]
-#   else
-#     partition_qubits = [[(c - 1) * nrow + r for c in 1:ncol] for r in 1:nrow]
-#   end
-#   partition_gates = [filter(g -> all(q -> q in p, g[2]), gates) for p in partition_qubits]
-#   return partition_gates
-# end
 
 function line_partition_gates(N, gates; order=1)
   @assert order in [1, 2, 3, 0]
@@ -95,24 +88,18 @@ function random_circuit_tn_line_partition_tree(
   partition_gates = []
   for (i, l) in enumerate(layers)
     push!(partition_gates, line_partition_gates(N, l; order=i % 4)...)
-    # if i % 4 in [0, 1]
-    #   push!(partition_gates, line_partition_gates(N, l; order="row")...)
-    # else
-    #   push!(partition_gates, line_partition_gates(N, l; order="column")...)
-    # end
   end
   hilbert = qubits(prod(N))
-  tensor_partitions = [productstate(hilbert)[:]]#[Vector{ITensor}([ITensor([1.0, 0.0], i) for i in hilbert])]
+  tensor_partitions = [productstate(hilbert)[:]]
   for gates in partition_gates
     tensors, hilbert = gate_tensors(hilbert, gates)
     push!(tensor_partitions, tensors)
   end
-  # tensor_partitions = [vectorize(tensor_partitions)]
   for gates in reverse(partition_gates)
     tensors, hilbert = gate_tensors(hilbert, reverse(gates); applydag=true)
     push!(tensor_partitions, tensors)
   end
-  push!(tensor_partitions, productstate(hilbert)[:])#Vector{ITensor}([ITensor([1.0, 0.0], i) for i in hilbert]))
+  push!(tensor_partitions, productstate(hilbert)[:])
   return line_network(tensor_partitions)
 end
 
